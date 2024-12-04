3da08fc5247376454e5e960b6a154bd4e17c7eda
Victor Hugo  Marques <victorhm@outlook.com>
2024-12-04 13:43:07 -0300

    Player Animations and Fix Tileset
    Player animations with flip on code
    Remove tiles from empty area

--- _starter_content/_demo/player_animation_player.gd
+++ _starter_content/_demo/player_animation_player.gd
@@ -1,48 +1,32 @@
 extends AnimationPlayer
 
 @onready var player = $".."
-
-var last_direction = "left"
+@onready var sprite_2d: Sprite2D = $"../Sprite2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.state_changed.connect(_on_state_changed)
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("player_attack"):
-		play("attack_" + last_direction)
-		player.can_move = false
-
 func _on_state_changed(state: String, velocity: Vector2):
 	
-	#IDLE
+	if velocity.x < 0:
+		sprite_2d.flip_h = true
+	elif velocity.x > 0:
+		sprite_2d.flip_h = false
+
 	if state == "idle":
 		play("idle")
-	#WALK
 	elif state == "walk":
-		if velocity.x < 0 and has_animation("walk_left"):
-			last_direction = "left"
-			play("walk_left")
-		elif velocity.x > 0 and has_animation("walk_right"):
-			last_direction = "right"
-			play("walk_right")
-	#JUMP
+		play("walk")
 	elif state == "jump":
 		# JUMP UP
 		if velocity.y > 0:
-			if velocity.x < 0 and has_animation("up_left"):
-				last_direction = "left"
-				play("up_left")
-			elif velocity.x > 0 and has_animation("up_right"):
-				last_direction = "right"
-				play("up_right")
+			play("up")
 		# FALLING DOWN
 		elif velocity.y < 0:
-			if velocity.x < 0 and has_animation("down_left"):
-				last_direction = "left"
-				play("down_left")
-			elif velocity.x > 0 and has_animation("down_right"):
-				last_direction = "right"
-				play("down_right")
+			play("down")
 	elif state == "attack":
-		play("attack_" + last_direction)
+		if velocity.y != 0:
+			play("attack", -1, 2)
+		else:
+			play("attack")
