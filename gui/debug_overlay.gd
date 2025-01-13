826e2e82d4fda337d4e2b8c55fa950593a357578
Victor Hugo  Marques <victorhm@outlook.com>
2025-01-13 15:50:21 -0300

    Fix ui
    Hide unecessary stuff (menu and debug)

--- gui/debug_overlay.gd
+++ gui/debug_overlay.gd
@@ -2,7 +2,7 @@ class_name DebugOverlay extends CanvasLayer
 
 @onready var rich_text_label: RichTextLabel = $RichTextLabel
 
-@export var ON:= true
+@export var ON:= false
 
 func show_message(message: String):
 	rich_text_label.text = message
---
+++ props/interactable/load_level_door.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_interactable_switch_interact(body: Node2D) -> void:
+	pass # Replace with function body.
