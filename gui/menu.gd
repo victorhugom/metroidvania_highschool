aac2a42ef426b016b801de18b30a8d2c72a06362
Victor Hugo  Marques <victorhm@outlook.com>
2025-02-03 13:28:59 -0300

    Fix menu go back
    When go back is selected goes to the previous menu

--- gui/menu.gd
+++ gui/menu.gd
@@ -8,13 +8,13 @@ const MENU_ITEM = preload("res://gui/menu_item.tscn")
 @onready var sub_container: HFlowContainer = $SubContainer
 @onready var selected_item_label: RichTextLabel = $CenterContainer/SelectedItemLabel
 @onready var money_label: RichTextLabel = $MoneyLabel
-@onready var exit_button: Button = $ExitButton
 
 var is_menu_open:bool = false
 var item_selected: MenuItem
 var item_selected_index: int = 0
 var menu_items: Array[Node]
 var current_menu: HFlowContainer
+var previous_menu_index: int = 0  # To store the index of the selected item in the main menu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,15 +26,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	rich_text_label_time.text = get_current_time()
-	if exit_button:
-		exit_button.visible = is_menu_open
 
 func get_current_time() -> String:
 	var now = Time.get_time_dict_from_system()
 	return "%d:%d:%d" % [now.hour, now.minute, now.second]
 	
 func _input(event: InputEvent) -> void:
-	
 	if event.is_action_released("ui_menu"):
 		if is_menu_open:
 			animation_player.play("hide_phone")
@@ -47,7 +44,7 @@ func _input(event: InputEvent) -> void:
 	if is_menu_open:
 		if event.is_action_released("ui_left"):
 			item_selected_index -= 1
-			if item_selected_index < 0: item_selected_index = 0
+			if item_selected_index < 0: item_selected_index = menu_items.size() - 1
 			select_item(item_selected_index)
 
 		if event.is_action_released("ui_right"):
@@ -58,18 +55,13 @@ func _input(event: InputEvent) -> void:
 		if event.is_action_pressed("ui_select"):
 			select_menu_item(item_selected.inventory_item.item_type)
 
-			
-func select_item(item_index:int):
-
+func select_item(item_index: int):
 	item_selected.selected = false
 	item_selected = menu_items[item_index]
 	item_selected.selected = true
-	
 	selected_item_label.text = item_selected.inventory_item.description
 
 func select_menu_item(menu_item_type: String):
-	
-	item_selected_index = 0
 	var player: Player = get_tree().get_nodes_in_group("player")[0]
 	
 	if menu_item_type == "back":
@@ -77,10 +69,15 @@ func select_menu_item(menu_item_type: String):
 		sub_container.visible = false
 		current_menu = menu_container
 		menu_items = current_menu.get_children()
+		item_selected_index = previous_menu_index  # Restore the previous menu index
 		select_item(item_selected_index)
 		return
+	
 	if current_menu == sub_container:
 		use_inventory_item()
+		return
+	
+	previous_menu_index = item_selected_index  # Store the current menu index before switching to the submenu
 	
 	var inventory = player.inventory
 	var inventory_items = inventory.get_items_grouped()
@@ -104,6 +101,7 @@ func select_menu_item(menu_item_type: String):
 	sub_container.visible = true
 	current_menu = sub_container
 	menu_items = current_menu.get_children()
+	item_selected_index = 0  # Reset the index when entering the submenu
 	select_item(item_selected_index)
 
 func use_inventory_item():
@@ -123,5 +121,10 @@ func use_inventory_item():
 
 	if is_item_used:
 		player.inventory.remove_item(inventory_item)
-		menu_container.remove_child(item_selected)
-		item_selected = menu_items[0]
+		menu_items.erase(item_selected)
+		item_selected.queue_free()
+		if menu_items.size() > 0:
+			item_selected_index = 0
+			select_item(item_selected_index)
+		else:
+			selected_item_label.text = "No items available"
